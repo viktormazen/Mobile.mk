ccd35ff7f7b76b4bd21bbaf07916bb9a8e27e4a5
Viktor Mijalchev <47793886+viktormazen@users.noreply.github.com>
2019-10-28 11:34:56 +0100

    4.1 fix

---
+++ mobile.mk/Controllers/VehiclesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mobile.DataAccess;
+using mobile.Domain.Enums;
+using mobile.Domain.Models;
+using mobile.Services.Interfaces;
+using mobile.WebModels;
+
+namespace mobile.WebApp.Controllers
+{
+    [Authorize]
+    public class VehiclesController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnviorment;
+        private readonly IVehicleService _vehicleService;
+
+
+
+        public VehiclesController(IVehicleService vehicleService, 
+                                  IHostingEnvironment hostingEnviorment)
+        {
+            _hostingEnviorment = hostingEnviorment;
+            _vehicleService = vehicleService;
+        }
+
+        // GET: Vehicles
+        [AllowAnonymous]
+        public IActionResult Index(string searchBy, string search)
+        {
+            bool test = User.Identity.IsAuthenticated;
+
+            if(searchBy == "Make")
+            {
+                return View(_vehicleService.GetAllVehicles().Where(v => v.Make.StartsWith(search)));
+            }
+            else if(searchBy == "Model")
+            {
+                return View(_vehicleService.GetAllVehicles().Where(v => v.Model.StartsWith(search)));
+            }
+            return View(_vehicleService.GetAllVehicles()); 
+        }
+
+        [Authorize(Roles = "customer")]
+        public IActionResult ListUserVehicles()
+        {
+            ViewBag.RenderActions = true;
+
+            return View("Index", _vehicleService.GetUserVehicles(User.Identity.Name));
+        }
+
+        [AllowAnonymous]
+        public IActionResult Details(int id)
+        {
+            var vehicle = _vehicleService.GetVehicleById(id);
+            return View(vehicle);
+        }
+
+        [Authorize(Roles = "customer, admin")]
+        public IActionResult Delete(int id)
+        {
+             _vehicleService.RemoveVehicle(id);
+            return View(nameof(Delete));
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = "customer")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "customer")]
+        public IActionResult Create(VehicleCreateViewModel vehicle)
+        {
+            _vehicleService.CreateVehicle(vehicle);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = "customer, admin")]
+        public IActionResult Edit(int id)
+        {
+            return View(_vehicleService.GetVehicleById(id));
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "customer, admin")]
+        public IActionResult Edit(VehicleViewModel vehicle)
+        {
+            _vehicleService.UpdateVehicle(vehicle);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
